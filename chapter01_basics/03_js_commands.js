f5e6660e5ca55c54cafb73b9ae1a98af0ce440c8
abbcreation <bibhu38.das@gmail.com>
2026-07-25 06:55:00 +0530

    Add OS detection code in 03_js_commands.js

---
+++ chapter01_basics/03_js_commands.js
@@ -0,0 +1,9 @@
+const os = require('os');
+//console.log("OS Type (os.type):", os.type());
+//console.log("OS Platform (os.platform):", os.platform());
+//console.log("OS Release:", os.release());
+
+console.log("OS Platform (process.platform):", process.platform);
+console.log("OS Arch (os.arch):", os.arch());
+
+
